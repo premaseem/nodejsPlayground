facd6eb3303559f332a462a0219f12c60a962788
Aseem Jain <Aseem.Jain@rackspace.com>
2017-03-13 15:23:08 -0500

    added call back to get ip address of domian

--- concepts/callBack.js
+++ concepts/callBack.js
@@ -1,8 +1,22 @@
-var http = require('http');
+//Basic get request using node
 
-http.request({ hostname: 'example.com' }, function(res) {
+var http = require('http');
+var dns = require('dns');
+
+// call back function is last argument for this function
+dns.resolve4('www.google.com', function (err, addresses) {
+  if (err) throw err;
+  console.log('addresses: ' + JSON.stringify(addresses));
+});
+
+
+cbf = function(res) {
   res.setEncoding('utf8');
   res.on('data', function(chunk) {
     console.log(chunk);
   });
-}).end();
+}
+
+http.request({ hostname: 'example.com' },cbf ).end();
+
+
