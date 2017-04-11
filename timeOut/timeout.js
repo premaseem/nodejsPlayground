1ac204d911b3b1994b0c40a183de5e25832c82f9
Aseem Jain <Aseem.Jain@rackspace.com>
2017-04-11 09:05:08 -0500

    Node JS Express app

---
+++ timeOut/timeout.js
@@ -0,0 +1,10 @@
+function printHello(){
+    console.log( "Hello, World!");
+}
+// Now call above function after 2 seconds
+setTimeout(printHello, 2000);
+
+// Now call above function after 2 seconds
+setInterval(printHello, 2000);
+
+console.log("Aseem Jain ")
